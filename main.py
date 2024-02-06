48ae18834687d754d0aec209921b4aa798a6d210
loken156 <jonathan.karlsson@mail.com>
2024-02-06 11:48:08 +0100

    added a similar word detecting function

--- main.py
+++ main.py
@@ -17,6 +17,25 @@ def extract_keywords(text):
     keywords = [token.text for token in doc if not token.is_stop and token.is_alpha]
     return keywords
 
+
+#Extraherar lika ord ifrån texterna
+def extract_similar_words(text1, text2):
+    doc1 = nlp(text1)
+    doc2 = nlp(text2)
+
+    # Extraherar unika tokens från texterna
+    tokens_text1 = set(token.text.lower() for token in doc1 if token.is_alpha and not token.is_stop)
+    tokens_text2 = set(token.text.lower() for token in doc2 if token.is_alpha and not token.is_stop)
+
+    # Hittar vilka ord som matchar
+    similar_words = tokens_text1.intersection(tokens_text2)
+
+    #Hittar common words i texterna och sparar i variabel
+    common_words1 = [token.text for token in doc1 if token.text.lower() in similar_words]
+    common_words2 = [token.text for token in doc2 if token.text.lower() in similar_words]
+
+    return common_words1, common_words2
+
 while True:
     #Ber användaren mata in 2 texter
     text1 = input('Input your first text here: ')
@@ -30,20 +49,25 @@ while True:
     while True:
         print('Choose what you want to do')
         print('1. Show similarity score')
-        print('2. Extract meaningful words')
-        print('3. Enter new texts')
+        print('2. Show similar words')
+        print('3. Extract meaningful words')
+        print('4. Enter new texts')
         print('0. Quit')
-        choice = input('Enter your choice (1, 2, 3 or 0): ')
+        choice = input('Enter your choice (1, 2, 3, 4 or 0): ')
 
         if choice == '1':
             print(f"Similarity score between the texts: {similarity_score}")
-        elif choice == '2':
+            continue
+        if choice == '2':
+            similar_words = extract_similar_words(text1, text2)
+            print("Similar Words:", similar_words)
+        elif choice == '3':
             #skapar variabler med de extractade keywordsen
             keywords_text1 = extract_keywords(text1)
             keywords_text2 = extract_keywords(text2)
             print("Keywords in Text 1: ", keywords_text1)
             print("Keywords in Text 2: ", keywords_text2)
-        elif choice == '3':
+        elif choice == '4':
             break
         elif choice == '0':
             exit()
