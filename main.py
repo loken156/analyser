fe4adf293683cd0b5e7724391860f374edee74d5
loken156 <jonathan.karlsson@mail.com>
2024-02-06 11:22:18 +0100

    added keyword function and a loop-menu

--- main.py
+++ main.py
@@ -10,11 +10,42 @@ def similarity(text1, text2):
     #Beräknar likheter mellan dokumenten
     similarity_score = doc1.similarity(doc2)
     return similarity_score
-#Ber användaren mata in 2 texter
-text1 = input('Input your first text here')
-text2 = input('Input your second text here')
+
+# Funktion för att extrahera nyckelord
+def extract_keywords(text):
+    doc = nlp(text)
+    keywords = [token.text for token in doc if not token.is_stop and token.is_alpha]
+    return keywords
+
+while True:
+    #Ber användaren mata in 2 texter
+    text1 = input('Input your first text here: ')
+    text2 = input('Input your second text here: ')
 
 
-similarity_score = similarity(text1, text2)
+    #skapar en variabel som storear likhetspoäng.
+    similarity_score = similarity(text1, text2)
 
-print(similarity_score)
+    #Printar en meny med olika alternativ för att analysera texten.
+    while True:
+        print('Choose what you want to do')
+        print('1. Show similarity score')
+        print('2. Extract meaningful words')
+        print('3. Enter new texts')
+        print('0. Quit')
+        choice = input('Enter your choice (1, 2, 3 or 0): ')
+
+        if choice == '1':
+            print(f"Similarity score between the texts: {similarity_score}")
+        elif choice == '2':
+            #skapar variabler med de extractade keywordsen
+            keywords_text1 = extract_keywords(text1)
+            keywords_text2 = extract_keywords(text2)
+            print("Keywords in Text 1: ", keywords_text1)
+            print("Keywords in Text 2: ", keywords_text2)
+        elif choice == '3':
+            break
+        elif choice == '0':
+            exit()
+        else:
+            print("Invalid choice")
